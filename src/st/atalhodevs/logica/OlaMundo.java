c53a8f760791e46c51bbf05f903d7d5871e12d67
henilcioterras <henilcioterras@gmail.com>
2023-09-08 16:30:58 +0000

    First commit

---
+++ src/st/atalhodevs/logica/OlaMundo.java
@@ -0,0 +1,7 @@
+package st.atalhodevs.logica;
+
+public class OlaMundo {
+	public static void main(String[] args) {
+		System.out.print("Olá, mundo!");
+	}
+}
---
+++ src/st/atalhodevs/logica/Variaveis.java
@@ -0,0 +1,27 @@
+package st.atalhodevs.logica;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		String nome="Ana";
+		
+		System.out.println("Nome da pessoa é: "+nome);
+		
+		int idade=18;
+		
+		System.out.println("Idade: "+idade);
+		
+		double peso=70.5;
+		
+		System.out.println("Peso: "+peso);
+		
+		float x=4.5f;
+		
+		System.out.println("Troco: "+x);
+		
+		char sexo='F';
+		
+		System.out.println("Sexo é: "+sexo);
+	}
+
+}
