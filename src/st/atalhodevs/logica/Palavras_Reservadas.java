3b8cdb9dc60eddb6e9887da243d5578e6937d1a7
henilcioterras <henilcioterras@gmail.com>
2023-11-16 09:04:03 +0000

    Break & Continue

---
+++ src/st/atalhodevs/logica/Palavras_Reservadas.java
@@ -0,0 +1,17 @@
+package st.atalhodevs.logica;
+
+public class Palavras_Reservadas {
+
+	public static void main(String[] args) {
+		
+		//break - serve para sair do loop 
+		//continue - serve para avancar apos atingir uma condicao
+		
+		for(int i=0;i<10;i++) {
+			if(i==4)continue;	
+			System.out.println(i);			
+		}
+
+	}
+
+}
